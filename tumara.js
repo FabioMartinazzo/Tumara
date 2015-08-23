d591fefaede99144a9f980073a6fc8c310ce3d94
Fábio Martinazzo <fabiomartinazzo@gmail.com>
2015-08-22 21:52:29 -0300

    Commit inicial

---
+++ tumara.js
@@ -0,0 +1,59 @@
+var headers = require('./node_modules/core/headers')
+var accesscontrol = require('./node_modules/core/accesscontrol')
+var config = require('./node_modules/core/config')
+var model = require('./node_modules/core/model')
+var selfws = require('./node_modules/webservice/selfwebservice')
+var following = require('./node_modules/core/following')
+var rollback = require('./node_modules/core/rollback')
+var error = require('./node_modules/error/error')
+var privbypass = require('./node_modules/privilege/privbypass')
+
+var Tumara = {
+	app : null,
+	init : function(app) {
+		this.app = app
+		error.init()
+	},
+	setAccessControl : function() {
+		accesscontrol.allowself(this.app)
+	},
+	setHeaders : function() {
+		this.app.use(headers.bodyParser.urlencoded( { extended : true } ))
+		this.app.use(headers.methodOverride())
+		this.app.use(headers.express.static(headers.path.join(__dirname, "public")))
+		this.app.use(headers.errorHandler( { dumpExceptions: true, showStack: true } ))
+	}
+}
+
+module.exports = {
+	init : function(callback) {
+		config.init(function() { 
+			Tumara.init(config.getConfiguration().getApp())
+			Tumara.setAccessControl()
+			Tumara.setHeaders()
+			privbypass.init()
+
+			callback()
+
+			config.getConfiguration().getApp().listen(config.getConfiguration().getPort())
+		})
+	},
+	getConfig : function() {
+		return myConfig
+	},
+	getModel : function() {
+		return model
+	},
+	handleError : function(errorCode, detail, callback) {
+		return callback(require('./node_modules/error/error').dispatch(errorCode, detail, true))
+	},
+	getSelf : function() {
+		return selfws
+	},
+	getFollower : function(obj) {
+		return following.getCurrent(obj)
+	},
+	setRollback : function(obj, entity, id, usuario, senha) {
+		rollback.setRollback(obj, entity, id, usuario, senha)
+	}
+}
